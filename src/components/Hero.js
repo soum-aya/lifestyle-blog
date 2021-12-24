4193b163e2bdb6cddd4890e9b2ccdc68940065db
Soumaya <soumaya.assadiki@gmail.com>
2021-12-24 15:03:58 +0100

    update hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,56 +4,58 @@ import { FaCaretRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useGlobalContext } from "../context";
 
-const heroUrl = "http://localhost/wordpress/wp-json/wp/v2/posts?tags=9";
-const url2 = "http://localhost/wordpress/wp-json/wp/v2/media/10";
+const tagsUrl = "http://localhost/wordpress/wp-json/wp/v2/tags";
+const heroUrl = "http://localhost/wordpress/wp-json/wp/v2/posts?tags=";
+const mediaUrl = "http://localhost/wordpress/wp-json/wp/v2/media/";
 
 function Hero() {
   const [heroPost, setHeroPost] = useState(null);
-  // const [imgUrl, setImgUrl] = useState([]);
+  const [heroImg, setHeroImg] = useState(null);
   const { logoDetails } = useGlobalContext();
 
-  const fetchHeropost = async () => {
+  const fetchTags = async () => {
     try {
-      const response1 = await fetch(heroUrl);
-      const heroPost = await response1.json();
-      if (heroPost) {
-        setHeroPost(heroPost);
-      } else {
-        setHeroPost(null);
-      }
+      const response = await fetch(tagsUrl);
+      const tags = await response.json();
 
-      console.log(heroPost);
+      const heroTag = tags.filter((tag) => tag.name === "hero");
+      const response1 = await fetch(`${heroUrl}${heroTag[0].id}`);
+      const heroPost = await response1.json();
+      setHeroPost(heroPost);
+
+      const response2 = await fetch(`${mediaUrl}${heroPost[0].featured_media}`);
+      const heroImg = await response2.json();
+      setHeroImg(heroImg);
     } catch (error) {
       console.log(error);
     }
   };
 
   useEffect(() => {
-    fetchHeropost();
+    fetchTags();
   }, []);
-  // const { id } = heroPost;
+
   if (!heroPost) {
     return <p>no hero post</p>;
   }
   return (
     <section className="hero">
       <div className="hero-center">
-        <p>{heroPost.title.rendered}</p>
-        {/* <article className="hero-container">
+        <article className="hero-container">
           <div className="hero-info">
             <img src={logoDetails.source_url} className="hero-logo" alt="logo" />
             <div className="hero-post">
-              <h2 className="hero-title">{heroPost.title.rendered}</h2>
-              <div dangerouslySetInnerHTML={{ __html: heroPost.content.rendered.substring(0, 249) }} className="hero-desc"></div>
+              <h2 className="hero-title">{heroPost[0].title.rendered}</h2>
+              <div dangerouslySetInnerHTML={{ __html: heroPost[0].content.rendered.substring(0, 200) }} className="hero-desc"></div>
 
-              <Link to={`/post/${id}`} className="read-more read-more-hero ">
+              <Link to={`/post/${heroPost[0].id}`} className="read-more read-more-hero ">
                 read more
                 <FaCaretRight style={{ marginLeft: "6px", fontSize: "20px" }} />
               </Link>
             </div>
           </div>
-        </article> */}
-        {/* <div className="hero-image" style={{ backgroundImage: `url(${imgUrl.source_url})` }}></div> */}
+        </article>
+        <div className="hero-image" style={{ backgroundImage: `url(${heroImg.source_url})` }}></div>
       </div>
     </section>
   );
