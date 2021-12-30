fb626d5ba997e4025d9f37e2977d3c3a9206acdc
Soumaya <soumaya.assadiki@gmail.com>
2021-12-30 22:22:41 +0100

    update postDetails component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { social } from "../data";
 import { FaBars, FaSearch, FaCaretDown, FaTimes } from "react-icons/fa";
-import logo from "../images/logo.svg";
 import { useGlobalContext } from "../context";
 import { Link } from "react-router-dom";
 
--- src/pages/PostDetails.js
+++ src/pages/PostDetails.js
@@ -7,23 +7,18 @@ const mediaUrl = "http://localhost/wordpress/wp-json/wp/v2/media/";
 const PostDetails = () => {
   const { id } = useParams();
   const [post, setPost] = useState(null);
-  const [postImg, setPostImg] = useState(null);
+  const [postImg, setPostImg] = useState({});
 
   useEffect(() => {
     const fetchPostDetails = async () => {
       try {
-        const post = await fetch(`${postUrl}${id}`).then((res) => res.json());
-        if (post) {
-          setPost(post);
-          const postImg = await fetch(`${mediaUrl}${post.featured_media}`).then((res) => res.json());
-          if (postImg) {
-            setPostImg(postImg);
-          } else {
-            setPostImg(null);
-          }
-        } else {
-          setPost(null);
-        }
+        const response = await fetch(`${postUrl}${id}`);
+        const post = await response.json();
+        setPost(post);
+
+        const response2 = await fetch(`${mediaUrl}${post.featured_media}`);
+        const postImg = await response2.json();
+        setPostImg(postImg);
       } catch (error) {
         console.log(error);
       }
@@ -36,7 +31,7 @@ const PostDetails = () => {
   }
   return (
     <section className="px-24 flex">
-      {/* <div className="h-[800px] bg-center bg-no-repeat bg-cover flex-1" style={{ backgroundImage: `url(${postImg.source_url})` }}></div> */}
+      <div className="h-[800px] bg-center bg-no-repeat bg-cover flex-1" style={{ backgroundImage: `url(${postImg.source_url})` }}></div>
       <div className="flex-1 ml-20 flex flex-col items-start justify-center space-y-6">
         <h2 className="text-3xl tracking-widest font-bold uppercase">{post.title.rendered}</h2>
         <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} className="hero-desc"></div>
